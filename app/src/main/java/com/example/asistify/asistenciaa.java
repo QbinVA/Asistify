b9f13df9423d07ed931f21a8d3353d057fc8c0d5
QbinVA <valdokevin02@gmail.com>
2024-04-19 21:35:13 -0600

    cree el archivo de asistenciaa

---
+++ app/src/main/java/com/example/asistify/asistenciaa.java
@@ -0,0 +1,14 @@
+package com.example.asistify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class asistenciaa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asistenciaa);
+    }
+}
