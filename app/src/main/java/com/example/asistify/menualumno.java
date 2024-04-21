2d2eca11d3019dbe78aca088ddc6c998d7762a22
QbinVA <valdokevin02@gmail.com>
2024-04-21 17:07:30 -0600

    creé el boton para unirse a una clase en menualumno

--- app/src/main/java/com/example/asistify/menualumno.java
+++ app/src/main/java/com/example/asistify/menualumno.java
@@ -1,8 +1,14 @@
 package com.example.asistify;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 public class menualumno extends AppCompatActivity {
 
@@ -10,5 +16,51 @@ public class menualumno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menualumno);
+
+        Button btnUnirse = findViewById(R.id.btnUnirse);
+
+        btnUnirse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Crear y mostrar el menú emergente
+                PopupMenu popupMenu = new PopupMenu(menualumno.this, btnUnirse);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_main, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        //Manejar los clics en las opciones del menu
+                        if (item.getItemId() == R.id.option_dialog) {//Mostrar el dialogo emergente cuando se hace clic en esta opcion
+                            showAlertDialog();
+                            return true;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                });
+                popupMenu.show();
+            }
+        });
     }
+
+    //Metodo para mostrar el dialogo emergente
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alerta")
+                .setMessage("Esta funcion no ha sido terminada.")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Accion cuando hace click en Aceptar
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Acciom cuando se hace clic en Cancelar
+                    }
+                })
+                .show();
+    }
+
 }
