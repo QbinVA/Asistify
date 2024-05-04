aedb2e7affbc204ff6506cc2cfb4eac713a9b636
unknown <Alan Isael@LAPTOP-3CR9SL76.LAPTOP-3CR9SL76>
2024-05-04 16:55:53 -0600

    personalice un poco

--- app/src/main/java/com/example/asistify/logosplash.java
+++ app/src/main/java/com/example/asistify/logosplash.java
@@ -3,10 +3,42 @@ package com.example.asistify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class logosplash extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1300; // 1.3 segundos
-    
+
+
+    protected void onCreate(Bundle saveInstancesState){
+        super.onCreate(saveInstancesState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_logosplash);
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.despla_arriba);
+
+        ImageView buho = findViewById(R.id.imgview3);
+
+        buho.setAnimation(animacion1);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(logosplash.this, loginalumno.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2300);
+
+
+    }
+
 }
