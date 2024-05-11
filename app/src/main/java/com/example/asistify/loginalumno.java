8878466858b4ce670a8ae7907c4069a204083e58
QbinVA <valdokevin02@gmail.com>
2024-05-11 13:52:36 -0600

    ya funciona la barra lateral

--- app/src/main/java/com/example/asistify/loginalumno.java
+++ app/src/main/java/com/example/asistify/loginalumno.java
@@ -58,7 +58,7 @@ public class loginalumno extends AppCompatActivity {
         buttonregistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(loginalumno.this, activity_registro.class);
+                Intent intent = new Intent(loginalumno.this, menualumno.class);
                 startActivity(intent);
             }
         });
